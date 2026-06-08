6081654b0b26cfa876486624d9e393ac0c7ebd0a
dylan18x <gadieldylan02@gmail.com>
2026-06-08 03:33:42 -0500

    Feat: Agregar relacion user a Reader para asignación manual

--- backend-fernandez-biblioteca/biblioteca/models/reader.py
+++ backend-fernandez-biblioteca/biblioteca/models/reader.py
@@ -1,8 +1,16 @@
 
 from django.db import models
+from django.conf import settings
 
 
 class Reader(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='reader_profile',
+        null=True, 
+        blank=True
+    )
     full_name = models.CharField(max_length=100, unique=True)
     phone = models.CharField(max_length=20,blank=True, default='')
     email = models.CharField(max_length=50, unique=True)
